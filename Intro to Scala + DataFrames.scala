d0af36aab23d40d57ac4500417a9dcd3dab7cc13
wesbosse <wes.bosse@gmail.com>
2018-03-12 10:40:11 -0700

    adding scala notebook from databricks

---
+++ Intro to Scala + DataFrames.scala
@@ -0,0 +1,90 @@
+// Databricks notebook source
+// set a variable and a constant
+var firstName = "Wesley"
+val lastName = "Bosse"
+
+firstName.slice(0,2)
+
+// string concatination
+val fullName = firstName + " " + lastName
+
+// array creation and member addition
+
+myArray :+= 5
+
+// array methods
+myArray.tail
+myArray.size
+myArray.min
+myArray.max
+myArray.sum
+myArray.product
+
+// COMMAND ----------
+
+var myTuple = (1,2,3)
+myTuple._3
+
+// COMMAND ----------
+
+var me = Map(("firstName", firstName), ("lastName", lastName), ("age", 24))
+
+//me("middleName") //errors
+me.get("middleName") //returns None
+
+me += ("middleName" -> "Allen")
+for ((key, value) <- me) {
+  print(key + ": ")
+  println(value)
+}
+
+me.keys
+me.values
+
+// COMMAND ----------
+
+// var myBoolean = false
+if (myBoolean) {
+  print("true")
+} else if (2 == 3 || 2 == 2) {
+ print("second true") 
+} else {
+  print("else")
+}
+
+// COMMAND ----------
+
+def multiply(num1:Int, num2:Int): Int = {
+  return num1 * num2
+}
+
+multiply(6, 100)
+
+// COMMAND ----------
+
+def printFunc(string:String): Unit = {
+  println(string)
+}
+
+printFunc("Hey")
+
+// COMMAND ----------
+
+var inlineFunction = (name:String) => println(s"Hello, $name")
+
+// COMMAND ----------
+
+inlineFunction("hello")
+
+// COMMAND ----------
+
+def stripAnchor (url:String): String = {
+  var urlSplit = url.split('#')
+  return urlSplit(1)
+}
+
+stripAnchor("google.com#anchor")
+
+// COMMAND ----------
+
+
